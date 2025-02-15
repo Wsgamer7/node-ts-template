d9cfd5e8a279d50daf81a32d85ac83f18347307a
Shuo Wang <ws18022@outlook.com>
2025-02-15 22:12:14 +0800

    fix complie err

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,24 +1,13 @@
+import { fixImportsPlugin } from 'esbuild-fix-imports-plugin'
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
   entry: ['src/**/*.ts'], // keep all files structure
-  //   entry: ['src/index.ts'], // single file
+  //entry: ['src/index.ts'], // single file
   // sourcemap: true,
   outDir: 'dist',
   clean: true,
   format: 'esm',
   bundle: false,
-  plugins: [
-    {
-      name: 'alias-plugin',
-      renderChunk(_, { code }) {
-        if (this.format === 'esm') {
-          const regex = /from ["']@\/(.+)["']/g
-          return {
-            code: code.replace(regex, 'from "./$1.js"'),
-          }
-        }
-      },
-    },
-  ],
+  esbuildPlugins: [fixImportsPlugin()],
 })
