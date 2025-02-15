fc27305edba8288022af1c28f0686e3b0491b9e6
Shuo Wang <ws18022@outlook.com>
2025-02-15 10:15:18 +0800

    2

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   //   entry: ['src/**/*.ts'], // keep all files structure
   entry: ['src/index.ts'], // single file
-  sourcemap: false,
+  sourcemap: true,
   outDir: 'dist',
   clean: true,
   format: 'esm',
